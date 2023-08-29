298793b5a38f1e4bbc4a319155f3a5e32fffc348
m4thz12 <rm98832@fiap.com>
2023-08-29 11:05:38 -0300

    aaa

---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+export default function Section() {
+    return (
+      <>
+        <section>
+            <p><a href="/">Home Page</a></p>
+            <button onClick={()=> alert("Hello World")}>Click-ME!</button>
+        </section>
+      </>
+    )
+  }
+
+
